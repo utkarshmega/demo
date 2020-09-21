6f394a1d6ecb166d24057797b2b2f4ba3201600e
utkarshmega <utkarsheduc@gmail.com>
2020-09-21 09:51:26 +0530

    Added successfully

---
+++ src/main/java/com/javaPractice/App.java
@@ -0,0 +1,25 @@
+package com.javaPractice;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	private static final Logger LOG = LogManager.getLogger(App.class); 
+	public static void main(String[] args) {
+		
+		String message = "Hello, World";
+		LOG.debug(message + " Will be printed on Debug");
+		LOG.info(message + " Will be printed on Info");
+		LOG.warn(message + " Will be printed on Warn");
+		LOG.error(message + " Will be printed on Error");
+		LOG.fatal(message + " Will be printed on Fatal");
+		LOG.info("Appending string: {}.", message);
+		System.out.println(message);
+
+
+	}
+}
